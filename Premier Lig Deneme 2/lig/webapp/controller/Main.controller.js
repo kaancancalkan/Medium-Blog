12d0640aa7ed21b0b327e5078eaba05a2579a013
Kaan Can Calkan <cancalkaniletisim@gmail.com>
2023-03-08 18:19:30 +0300

    New Project Added

--- Premier Lig Deneme 2/lig/webapp/controller/Main.controller.js
+++ Premier Lig Deneme 2/lig/webapp/controller/Main.controller.js
@@ -1,17 +1,18 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     'sap/ui/model/json/JSONModel'
-    ],
+],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
     function (Controller,JSONModel) {
         "use strict";
 
-        return Controller.extend("statistics.premierleague.controller.Main", {
+        return Controller.extend("premier.lig.controller.Main", {
             onInit: function () {
-            var oModel = this.getOwnerComponent().getModel("League");
-            oModel.loadData(sap.ui.require.toUrl("statistics/premierleague/model/league.json"));
+                var oModel = this.getOwnerComponent().getModel("Student");
+                 oModel.loadData(sap.ui.require.toUrl("premier/lig/model/student.json"));
+
             }
         });
     });
---
+++ Premier Lig Deneme 2/lig/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["premier/lig/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
